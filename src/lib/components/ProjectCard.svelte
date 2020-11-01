d95b0383c318fc68f365ef6a7450f7e385b9185f
susan0907miller <susan0907miller@gmail.com>
2020-11-01 14:00:00 +0800

    modify style

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -6,6 +6,5 @@
 <div class="w-4/5 min-w-[200px]  bg-gray-800 text-white flex flex-col items-center p-4 sticky top-0">
   <h3 class="mb-4 text-center font-bold">{ product.name }</h3>
   <img src="/profile.png" alt="Profile" class="w-16 h-16 rounded-full mb-2"/>
-  
   <p>Designed and implemented a complete e-commerce platform with shopping cart, payment system, and product management features.</p>
 </div>
