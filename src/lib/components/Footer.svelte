255e07f33347502596e2eecb0f723dcd67be5dbc
susan0907miller <susan0907miller@gmail.com>
2020-11-01 14:00:00 +0800

    my portfolio by sveltekit

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,3 @@
+<footer class="bg-gray-800 text-white text-center p-4 sticky bottom-0">
+  &copy; 2026 Susan Miller. All rights reserved.
+</footer>
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  let darkMode = false;
+
+  function toggleDark() {
+    darkMode = !darkMode;
+    document.documentElement.classList.toggle('dark', darkMode);
+  }
+</script>
+
+<header class="flex justify-between items-center p-4 bg-blue-500 text-white sticky top-0 z-10 shadow">
+  <h2 class="text-2xl font-bold mb-2">Full Stack Developer</h2>
+  <!-- <input type="text" placeholder="Search..." class="p-2 rounded w-60"/>
+  <button on:click={toggleDark} class="px-4 py-2 rounded bg-blue-700">🌙</button> -->
+</header>
